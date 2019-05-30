2963b6035a984bbafee1121974c2b6eb01ad90e3
Níkolas Vargas <vargasnikolass@gmail.com>
2019-05-29 23:26:30 -0300

    caching urlrequest

--- parser.py
+++ parser.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
 import json
 import requests
+import warnings
 
-def _get_request(url: str) -> dict:
+URL = 'https://economia.awesomeapi.com.br/json/all'
+LOCAL_JSON = 'data/zxc.json'
+
+def _load():
+    if not Path(LOCAL_JSON).exists():
+        msg = 'downloading {}, to JSON'.format(URL, LOCAL_JSON)
+        warnings.warn(msg)
+        with open(LOCAL_JSON, 'wb') as local:
+            local.write(_get_request(URL))
+
+    with open(LOCAL_JSON) as json_file:
+        return json.load(json_file)
+
+
+def _get_request(url: str) -> bytes:
     request = requests.get(url)
-    data = json.loads(request.content)
-    return data
+    return request.content
+
 
 class Parser:
     def __init__(self, url:str):
-        self.data = _get_request(url)
+        self.data = json.loads(_get_request(url))
 
     def __repr__(self):
         return "Parser({})".format(self.data)
@@ -31,7 +47,4 @@ class Coin:
 
 if __name__ == "__main__":
     request = Parser('https://economia.awesomeapi.com.br/json/all')
-    c = Coin(request.data['USD']['name'], request.data['USD']['code'])
 
-    for k in request.data:
-        print("{} -> {}".format(k, request.data[k]))
